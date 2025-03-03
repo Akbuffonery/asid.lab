334752fa493a42b191e87d4a5ae64851260439e5
Akbuffonery <gumgim905@gmail.com>
2025-03-03 15:20:35 +0400

    Add files via upload

---
+++ lr2.py
@@ -0,0 +1,12 @@
+import re
+digit_words = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
+def num2words(num):
+    return ' '.join(digit_words[int(d)] for d in str(num))
+with open('input.txt', 'r') as f:
+    numbers = re.findall(r'\b\d+\b', f.read())
+    minzn = min(map(int, numbers))
+    maxzn = max(map(int, numbers))
+    for num_str in numbers:
+        if num_str.startswith('77') and len(num_str) > 2:
+            print(num_str[2:])
+print('Среднее число =', num2words((minzn + maxzn) // 2))
