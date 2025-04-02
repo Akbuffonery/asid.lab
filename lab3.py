7327a63984abef8a71662f474a1277cc180bd5bf
Akbuffonery <gumgim905@gmail.com>
2025-04-02 17:06:54 +0400

    lab3.py

--- lab3.py
+++ lab3.py
@@ -11,21 +11,23 @@ count = 0
 product = 1
 for i in range(n):
     for j in range(n):
-        if i > j and j > (n-1-i) and (j+1)%2 == 0 and F[i][j] > k:
+        if i > j and j > (n-1-i) and (j+1) % 2 == 0 and F[i][j] > k:
             count += 1
-        if i < j and j < (n-1-i) and (i+1)%2 == 1 and F[i][j]%2 == 0:
+        if i < j and j < (n-1-i) and (i+1) % 2 == 1 and F[i][j] % 2 == 0:
             product *= F[i][j]
 # Меняем F по условию
 if count < product:
+    # Меняем нижний правый треугольник с транспонированным
     for i in range(n):
         for j in range(n):
             if i > j and j > (n-1-i):
                 F[i][j], F[j][i] = F[j][i], F[i][j]
 else:
+    # Меняем верхний правый треугольник с повёрнутым
     for i in range(n):
         for j in range(n):
             if i < j and j > (n-1-i):
-                F[i][j], F[n-1-j][i] = F[n-1-j][i], F[i][j]
+                F[i][j], F[n-1-j][n-1-i] = F[n-1-j][n-1-i], F[i][j]
 # Транспонируем A
 AT = [[A[j][i] for j in range(n)] for i in range(n)]
 # Вычисляем k*A*F
