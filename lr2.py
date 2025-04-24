055e90002a1e60956b3c46d0fbdf79981f97440f
Akbuffonery <gumgim905@gmail.com>
2025-04-24 14:11:51 +0400

    lr2.py

--- lr2.py
+++ lr2.py
@@ -2,7 +2,7 @@ import re
 digit_words = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
 def num2words(num): return ' '.join(digit_words[int(d)] for d in str(num))
 with open('input.txt', 'r') as f:
-    numbers = re.findall(r'\b\d+\b', f.read())
+    numbers = re.findall(r'\b\d{0,5}\b', f.read())
     minzn, maxzn = min(map(int, numbers)), max(map(int, numbers))
     for num_str in numbers:
         if num_str.startswith('77') and len(num_str) > 2:
